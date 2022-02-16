bc69562d2e6af17be5cb918b2b26ed28be70e728
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-02-16 23:25:01 +0100

    Fixed hand size on mobile devices

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header role="banner" class="sticky top-0 bg-padlok-blue text-white overflow-hidden">
     <div class="container px-safe mx-auto flex justify-between items-center">
       <a :href="$t('url.marketing')" class="px-2" target="_blank">
-        <h1 class="py-2 text-4xl sm:text-5xl font-hkgrotesk-black">PADL<span class="text-padlok-cyan relative"><svg class="absolute top-6 w-9 sm:top-7 sm:left-0 sm:w-12 fill-skintone-0" viewbox="0 0 48 61"><use xlink:href="/images/hand.svg#Hand"/></svg>OK</span></h1>
+        <h1 class="py-2 text-4xl sm:text-5xl font-hkgrotesk-black">PADL<span class="text-padlok-cyan relative"><svg class="absolute top-6 w-9 sm:top-7 sm:left-0 sm:w-12 fill-skintone-0" viewBox="0 0 48 61"><use xlink:href="/images/hand.svg#Hand"/></svg>OK</span></h1>
       </a>
     </div>
   </header>
